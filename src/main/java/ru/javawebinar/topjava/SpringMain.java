1ea7c1aa7fadf457e2e07f24470ebe09b7f4792e
MaximLN <90178889+MaximLN@users.noreply.github.com>
2022-02-15 11:24:46 +0300

    add Spring Bean

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,13 +2,10 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.Arrays;
 
 public class SpringMain {
@@ -21,13 +18,6 @@ public class SpringMain {
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
             System.out.println("mealRestController.get(1): " + mealRestController.get(1));
-            System.out.println("mealRestController.getAll(): " + mealRestController.getAll());
-            mealRestController.delete(1);
-            System.out.println("mealRestController.delete(1)");
-            mealRestController.update(new Meal(2, 1, LocalDateTime.of(2021, Month.APRIL, 3, 20, 0), "Ужин Update appCtx.getBean", 1000), 2);
-            System.out.println("mealRestController.update()");
-            mealRestController.create(new Meal(null, null, LocalDateTime.of(2021, Month.MAY, 3, 20, 0), "Ужин Create appCtx.getBean", 1200));
-            System.out.println("mealRestController.create()");
         }
     }
 }
