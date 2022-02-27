e7f75b04ffaab5d55371f8a24e008d058f1ff545
MaximLN <90178889+MaximLN@users.noreply.github.com>
2022-02-27 21:48:34 +0300

    Added Stopwatch Rule

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,6 +1,10 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -8,6 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.ExecutionTime;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -29,6 +34,13 @@ public class MealServiceTest {
 
     @Autowired
     private MealService service;
+
+    @ClassRule
+    public static ExternalResource summary = ExecutionTime.SUMMARY;
+
+    @Rule
+    public Stopwatch stopwatch = ExecutionTime.STOPWATCH;
+
 
     @Test
     public void delete() {
