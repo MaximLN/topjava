46992cac088867fe2b6b27598038cb147e9fa43c
MaximLN <90178889+MaximLN@users.noreply.github.com>
2022-03-09 09:41:54 +0300

    Abstract Test Class

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceDataJpaPostgresTest.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.Profiles.POSTGRES_DB;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles({DATAJPA, POSTGRES_DB})
+
+public class MealServiceDataJpaPostgresTest extends MealServiceTest {
+}
