e17c459454e0eefe54bc6d7b648c6a0948513913
MaximLN <90178889+MaximLN@users.noreply.github.com>
2022-03-17 14:53:22 +0300

    Work on bugs

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -2,14 +2,12 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.web.RootController;
+import ru.javawebinar.topjava.service.MealService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
@@ -22,44 +20,40 @@ import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
-public class JspMealController {
-    private static final Logger log = LoggerFactory.getLogger(RootController.class);
+public class JspMealController extends AbstractMealController {
+    private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
 
-    @Autowired
-    private AbstractMealController abstractMealController;
+    public JspMealController(MealService service) {
+        super(service);
+    }
 
     @GetMapping("/meals")
-    public String doGet(
-            @RequestParam(required = false) String action,
-            @RequestParam(required = false) String id,
-            @RequestParam(required = false) String startDate,
-            @RequestParam(required = false) String endDate,
-            @RequestParam(required = false) String startTime,
-            @RequestParam(required = false) String endTime,
-            Model model) {
-        log.info("doGet meals");
+    public String doGet(HttpServletRequest request, Model model) {
+        log.info("Get meals");
+        model.addAttribute("meals", getAll());
         String result = "meals";
-        model.addAttribute("meals", abstractMealController.getAll());
-        switch (action == null ? "all" : action) {
+        model.addAttribute("meals", getAll());
+        switch (request.getParameter("action") == null ? "all" : request.getParameter("action")) {
             case "delete" -> {
-                delete(id, model);
+                delete(request.getParameter("id"), model);
                 return "redirect:meals";
             }
-            case "create", "update" -> result = createOrUpdate(id, action, model);
-            case "filter" -> filter(startDate, endDate, startTime, endTime, model);
+            case "create", "update" -> result = createOrUpdate(request.getParameter("id"), request.getParameter("action"), model);
+            case "filter" -> filter(request.getParameter("startDate"), request.getParameter("endDate"),
+                    request.getParameter("startTime"), request.getParameter("endTime"), model);
         }
         return result;
     }
 
     public void delete(String id, Model model) {
-        abstractMealController.delete(Integer.parseInt(id));
-        model.addAttribute("meals", abstractMealController.getAll());
+        delete(Integer.parseInt(id));
+        model.addAttribute("meals", getAll());
     }
 
     public String createOrUpdate(String id, String action, Model model) {
         final Meal meal = "create".equals(action) ?
                 new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                abstractMealController.get(Integer.parseInt(id));
+                get(Integer.parseInt(id));
         model.addAttribute("meal", meal);
         return "mealForm";
     }
@@ -69,18 +63,19 @@ public class JspMealController {
         LocalDate end_Date = parseLocalDate(endDate);
         LocalTime start_Time = parseLocalTime(startTime);
         LocalTime end_Time = parseLocalTime(endTime);
-        model.addAttribute("meals", abstractMealController.getBetween(start_Date, start_Time, end_Date, end_Time));
+        model.addAttribute("meals", getBetween(start_Date, start_Time, end_Date, end_Time));
     }
 
     @PostMapping("/meals")
-    public String doGet(HttpServletRequest request) {
+    public String doPost(HttpServletRequest request) {
         log.info("Post meals");
         if (Objects.equals(request.getParameter("id"), "")) {
-            abstractMealController.create(new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
+            create(new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
                     request.getParameter("description"), Integer.parseInt(request.getParameter("calories"))));
         } else {
-            abstractMealController.update(new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
-                    request.getParameter("description"), Integer.parseInt(request.getParameter("calories"))), Integer.parseInt(request.getParameter("id")));
+            update(new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
+                            request.getParameter("description"), Integer.parseInt(request.getParameter("calories"))),
+                    Integer.parseInt(request.getParameter("id")));
         }
         return "redirect:meals";
     }
